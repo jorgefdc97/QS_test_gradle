5f3ae3d21669d44b305c8c6a741654d891ba4281
jorgefdc <jorgefdc@outlook.com>
2023-10-11 19:20:10 +0100

    Issues importing DescriptiveStatistics

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
 public class Main {
     public static void main(String[] args) {
         double[] values = new double[] {65, 51 , 16, 11 , 6519, 191 ,0 , 98, 19854, 1, 32};
